2e09f18f89938f30bfa8a4f3de4aa9ffb7c0344c
gamingislevi <GitHub01!>
2024-09-20 21:03:26 +0200

    fixed bugs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,11 @@ namespace unlocker;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "44.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1";
     public const string Name = "unlocker";
 
-    public const string Description = "unlocker a mod so you kan unlock stuff";
+    public const string Description = "unlocker a mod so you kan unlock stuff:f1 wil give you every insta monkey:f2 wil unlock all monkey knowledge availble:f3 will give 1000000 monkey money and xp two alll towers pressing:f4 will give you 10000 trophies  Pressing:F5 will unlock all trophy items including exclusive ones.";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "gamingislevi"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "unlocker"; // TODO add your repo name here, also in the download url in README.md
 }
